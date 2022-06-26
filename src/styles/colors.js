e651c394f29c435d8f2f6d7f9cd83d503706182e
emachipana <emachipana0102@gmail.com>
2022-06-26 16:41:50 -0500

    ADD: color in gray when navbar is moving

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,8 @@
 const colors = {
   gray: {
     primary: "#232323",
-    secondary: "#494949"
+    secondary: "#494949",
+    move: "rgba(42, 42, 42, 0.9)"
   },
   red: "#EF6D4D",
   white: "#F5F5F5"
