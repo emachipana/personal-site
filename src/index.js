764eed89cd8304a069b85bba519ec1dc958688cd
emachipana <emachipana0102@gmail.com>
2022-06-26 18:25:29 -0500

    FIX: manage background color

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Global } from "@emotion/react";
-import { global } from './styles';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './context/theme-context';
+import { Global } from '@emotion/react';
+import { global } from "./styles"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Global styles={global}/>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
