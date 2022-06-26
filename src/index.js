d455a88d153ca1f063983b9e0eb9291fdcb952a4
emachipana <emachipana0102@gmail.com>
2022-06-26 16:42:32 -0500

    ADD: render by test navbar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Global } from "@emotion/react";
 import { global } from './styles';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Global styles={global}/>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
