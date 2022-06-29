f722e13cddad81e6b8d7cecd3ab15d0e6f1bb963
emachipana <emachipana0102@gmail.com>
2022-06-29 11:03:33 -0500

    ADD: size of data skills

--- src/data/en/skills.js
+++ src/data/en/skills.js
@@ -1,44 +1,64 @@
-import { SiJavascript, SiRubyonrails, SiPostgresql } from "react-icons/si";
+import { SiJavascript, SiRubyonrails } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
-import { DiRuby, DiGit, DiCss3 } from "react-icons/di";
+import { DiGit, DiCss3 } from "react-icons/di";
 import { TiHtml5 } from "react-icons/ti";
 import { BsGithub } from "react-icons/bs";
+import { DiRubyRough } from "react-icons/di";
 
 export const skills = [
   {
-    name: "React JS",
-    icon: <GrReactjs />
+    name: "React Js",
+    icon: <GrReactjs 
+            size="50px"
+            color="#61CFEE"
+          />
   },
   {
-    name: "Ruby on Rails",
-    icon: <SiRubyonrails />
+    name: "Rails",
+    icon: <SiRubyonrails 
+            size="50px"
+            color="#C22200"
+          />
   },
   {
-    name: "JavaScript",
-    icon: <SiJavascript />
+    name: "Javascript",
+    icon: <SiJavascript 
+            size="45px"
+            color="#F2CF3C"
+          />
   },
   {
     name: "Ruby",
-    icon: <DiRuby />
+    icon: <DiRubyRough 
+            size="45px"
+            color="#A81C02"
+          />
   },
   {
     name: "HTML5",
-    icon: <TiHtml5 />
+    icon: <TiHtml5 
+            size="50px"
+            color="#EB471C"
+          />
   },
   {
     name: "CSS3",
-    icon: <DiCss3 />
-  },
-  {
-    name: "PostgreSQL",
-    icon: <SiPostgresql />
+    icon: <DiCss3 
+            size="50px"
+            color="#4893D3"
+          />
   },
   {
     name: "Git",
-    icon: <DiGit />
+    icon: <DiGit 
+            size="50px"
+            color="#EB4E29"
+          />
   },
   {
     name: "Github",
-    icon: <BsGithub />
+    icon: <BsGithub 
+            size="50px"
+          />
   }
 ];
