66de4f1571566a5b10d9098ec267270da62b1385
emachipana <emachipana0102@gmail.com>
2022-06-29 00:20:08 -0500

    ADD: animation to about me

--- src/components/AboutMe/about-me.js
+++ src/components/AboutMe/about-me.js
@@ -1,21 +1,24 @@
 import { useTheme } from "../../context/theme-context";
 import * as Style from "./styles";
+import { Slide } from "react-reveal";
 
 function AboutMe() {
   const { theme } = useTheme();
 
   return (
     <Style.Container>
-      <Style.Title>About Me</Style.Title>
-      <Style.Text theme={theme}>
-          I'm a Full-Stack Web Developer 💻 with experience
-          building high quality websites 💪
-          I always try to be the one leading the team 🤓
-          I consider myself passionate
-          about technology and eternally
-          self-taught 🧠 I'm always open
-          to learning new technologies.
-      </Style.Text>
+      <Slide left>
+        <Style.Title>About Me</Style.Title>
+        <Style.Text theme={theme}>
+            I'm a Full-Stack Web Developer 💻 with experience
+            building high quality websites 💪
+            I always try to be the one leading the team 🤓
+            I consider myself passionate
+            about technology and eternally
+            self-taught 🧠 I'm always open
+            to learning new technologies.
+        </Style.Text>
+      </Slide>
     </Style.Container>
   );
 }
