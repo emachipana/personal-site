46e9cf9324430709b0bb625aeaf93cad91381554
emachipana <emachipana0102@gmail.com>
2022-06-29 09:57:39 -0500

    ADD: data of skills

---
+++ src/data/en/skills.js
@@ -0,0 +1,44 @@
+import { SiJavascript, SiRubyonrails, SiPostgresql } from "react-icons/si";
+import { GrReactjs } from "react-icons/gr";
+import { DiRuby, DiGit, DiCss3 } from "react-icons/di";
+import { TiHtml5 } from "react-icons/ti";
+import { BsGithub } from "react-icons/bs";
+
+export const skills = [
+  {
+    name: "React JS",
+    icon: <GrReactjs />
+  },
+  {
+    name: "Ruby on Rails",
+    icon: <SiRubyonrails />
+  },
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />
+  },
+  {
+    name: "Ruby",
+    icon: <DiRuby />
+  },
+  {
+    name: "HTML5",
+    icon: <TiHtml5 />
+  },
+  {
+    name: "CSS3",
+    icon: <DiCss3 />
+  },
+  {
+    name: "PostgreSQL",
+    icon: <SiPostgresql />
+  },
+  {
+    name: "Git",
+    icon: <DiGit />
+  },
+  {
+    name: "Github",
+    icon: <BsGithub />
+  }
+];
