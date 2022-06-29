cbc2bc071136408c85ef42c56f13b7ff029ccb98
emachipana <emachipana0102@gmail.com>
2022-06-29 11:02:39 -0500

    ADD: structure to my skills section

--- src/components/MySkills/my-skills.js
+++ src/components/MySkills/my-skills.js
@@ -1,5 +1,26 @@
-function MySkills() {
+import { useTheme } from "../../context/theme-context";
+import { skills } from "../../data/en/skills";
+import * as Styles from "./styles";
+import { LightSpeed } from "react-reveal";
 
+function MySkills() {
+  const { theme } = useTheme();
+  
+  return (
+    <Styles.Container>
+      <LightSpeed right>
+        <Styles.Title>My Skills</Styles.Title>
+        <Styles.Cards>
+          { skills.map(skill => (
+            <Styles.Card theme={theme} key={skill.name}>
+              { skill.icon }
+              <h2>{skill.name}</h2>
+            </Styles.Card>
+          )) }
+        </Styles.Cards>
+      </LightSpeed>
+    </Styles.Container>
+  );
 }
 
 export default MySkills;
