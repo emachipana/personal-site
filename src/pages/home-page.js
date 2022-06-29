4ed3f4431de5af81e5c9e124e266d9fa36082f34
emachipana <emachipana0102@gmail.com>
2022-06-29 11:03:18 -0500

    ADD: render in home page

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,13 +1,14 @@
 import AboutMe from "../components/AboutMe/about-me";
 import Hero from "../components/Hero";
+import MySkills from "../components/MySkills/my-skills";
 import { MainContainer } from "./styes";
-
 
 function HomePage() {
   return (
     <MainContainer>
       <Hero />
       <AboutMe />
+      <MySkills />
     </MainContainer>
   )
 };
