5d72b9c9c0026d852da473bbf54a8f277b4ca1a7
emachipana <emachipana0102@gmail.com>
2022-06-30 10:34:08 -0500

    ADD: projects component

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,9 +1,28 @@
+import { useTheme } from "../../context/theme-context";
+import { projects } from "../../data/projects";
 import * as Style from "./styles";
+import { Bounce } from "react-reveal";
 
 function Projects() {
+  const { theme } = useTheme();
+
   return (
     <Style.Container>
-      <Style.Title>My Projects</Style.Title>
+      <Bounce right>
+        <Style.Title>Portfolio</Style.Title>
+        <Style.Section>
+          { projects.map((project, index) => (
+            <Style.Card
+              key={index}
+              theme={theme}
+              to={project.to}
+            >
+              { project.icon }
+              { project.name }
+            </Style.Card>
+          )) }
+        </Style.Section>
+      </Bounce>
     </Style.Container>
   );
 }
