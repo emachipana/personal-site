3a49564ed20e60823622261180df36cdaa902a64
emachipana <emachipana0102@gmail.com>
2022-06-30 11:14:23 -0500

    ADD: title and container un styles pages

--- src/pages/styes.js
+++ src/pages/styes.js
@@ -1,5 +1,27 @@
 import styled from "@emotion/styled";
+import { colors } from "../styles";
 
 export const MainContainer = styled.div`
   margin: 0 1rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 50px;
+  color: ${colors.red};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 2rem 0;
+  flex-direction: ${({ direction }) => direction || "row"};
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  max-width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+
+  @media screen and (max-width: 720px) {
+    margin-top: 10vh;
+  }
 `;
