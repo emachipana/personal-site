9463bd61f11b1fc63d21f69bd09fbaf3f2c22b70
emachipana <emachipana0102@gmail.com>
2022-06-30 11:14:35 -0500

    ADD: not found component

---
+++ src/pages/notfound-page.js
@@ -0,0 +1,13 @@
+import * as Style from "./styes";
+
+function NotFound() {
+  return (
+    <Style.MainContainer>
+      <Style.Container>
+        <Style.Title>Page Not Found</Style.Title>
+      </Style.Container>
+    </Style.MainContainer>
+  );
+}
+
+export default NotFound;
