66d7ddc7e43efa911b224ba4d2ae30cdbff542a5
emachipana <emachipana0102@gmail.com>
2022-06-30 01:07:42 -0500

    ADD: projects in data

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    language: "Ruby",
+    to: "",
+    color: "",
+    icon: ""
+  },
+  {
+    language: "Rails",
+    to: "",
+    color: "",
+    icon: ""
+  },
+  {
+    language: "Javascript",
+    to: "",
+    color: "",
+    icon: ""
+  },
+  {
+    language: "React",
+    to: "",
+    color: "",
+    icon: ""
+  }
+]
