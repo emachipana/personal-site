69f1c2d0ac8ed98b3b14177b68a5edb7c339afe5
emachipana <emachipana0102@gmail.com>
2022-06-29 20:21:27 -0500

    ADD: some colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,19 +3,24 @@ const colors = {
     primary: "#232323",
     secondary: "#494949",
   },
+  card: "rgb(230, 230, 230)",
   red: "#EF6D4D",
   white: "#F5F5F5",
   dark: {
     background: "#232323",
     font: "#F5F5F5",
     move: "rgba(42, 42, 42, 0.9)",
-    card: "rgb(60, 60, 60)"
+    card: "rgb(60, 60, 60)",
+    footer: "#E76C4D",
+    footerFont: "#494949"
   },
   light: {
     background: "#F5F5F5",
     font: "#494949",
     move: "rgba(227, 227, 227, 0.9)",
-    card: "rgb(230, 230, 230)"
+    card: "rgb(230, 230, 230)",
+    footer: "#EF6D4D",
+    footerFont: "#F5F5F5"
   }
 }
 
