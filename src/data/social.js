6a990cbff5d026ade06dcd52e156fa861945fee6
emachipana <emachipana0102@gmail.com>
2022-06-29 19:53:50 -0500

    ADD: socials in data

---
+++ src/data/social.js
@@ -0,0 +1,35 @@
+import { 
+  BsGithub,
+  BsTwitter,
+  BsFacebook,
+  BsLinkedin } from "react-icons/bs";
+
+export const socials = [
+  {
+    to: "https://github.com/emachipana",
+    icon: <BsGithub 
+            size="30px"
+          />
+  },
+  {
+    to: "https://twitter.com/ChipanaEnmanuel",
+    icon: <BsTwitter 
+            size="30px"
+            color="#4FA4D9"
+          />
+  },
+  {
+    to: "https://www.facebook.com/enmanuel.chipana",
+    icon: <BsFacebook 
+            size="30px"
+            color="#4072E6"
+          />
+  },
+  {
+    to: "https://www.linkedin.com/in/enmanuel-chipana-araujo-44aa5a226",
+    icon: <BsLinkedin 
+            size="30px"
+            color="#3461B8"
+          />
+  }
+];
