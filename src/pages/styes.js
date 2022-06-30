af2a0934cf0bacf12348a271ca03a0657769b3f5
emachipana <emachipana0102@gmail.com>
2022-06-29 19:54:07 -0500

    REMOVE: styles of link

--- src/pages/styes.js
+++ src/pages/styes.js
@@ -1,17 +1,5 @@
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
-import { colors } from "../styles";
 
 export const MainContainer = styled.div`
   margin: 0 1rem;
-`;
-
-export const LinkTo = styled(Link)`
-  font-size: 45px;
-  color: ${colors.red};
-  font-weight: 700;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  justify-content: center;
 `;
