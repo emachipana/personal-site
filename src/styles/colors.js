ba6caffa566b5bf9404c54106f59a7ad6d8d57c1
emachipana <emachipana0102@gmail.com>
2022-06-29 20:30:56 -0500

    FIX: colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,7 +3,7 @@ const colors = {
     primary: "#232323",
     secondary: "#494949",
   },
-  card: "rgb(230, 230, 230)",
+  card: "#E6E6E6",
   red: "#EF6D4D",
   white: "#F5F5F5",
   dark: {
@@ -11,16 +11,14 @@ const colors = {
     font: "#F5F5F5",
     move: "rgba(42, 42, 42, 0.9)",
     card: "rgb(60, 60, 60)",
-    footer: "#E76C4D",
-    footerFont: "#494949"
+    footer: "#DA674B"
   },
   light: {
     background: "#F5F5F5",
     font: "#494949",
     move: "rgba(227, 227, 227, 0.9)",
     card: "rgb(230, 230, 230)",
-    footer: "#EF6D4D",
-    footerFont: "#F5F5F5"
+    footer: "#EF6D4D"
   }
 }
 
