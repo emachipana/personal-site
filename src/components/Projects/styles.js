e06df8cc0d5b01acaa21505d86cb8066b9b80c4a
emachipana <emachipana0102@gmail.com>
2022-06-30 10:34:32 -0500

    FIX: Card from div to Link react router

--- src/components/Projects/styles.js
+++ src/components/Projects/styles.js
@@ -1,15 +1,20 @@
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 import { colors } from "../../styles";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
+  margin: 2rem 0;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  gap: 20px;
+  gap: 2rem;
   overflow: hidden;
+  @media screen and (max-width: 720px) {
+    margin-top: 10vh;
+  }
 `;
 
 export const Title = styled.h1`
@@ -23,11 +28,25 @@ export const Section = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 1rem;
+  gap: 20px;
   flex-wrap: wrap;
 `;
 
-export const Card = styled.div`
-  background-color: ${({ color }) => color};
-  color: ${colors.white};
+export const Card = styled(Link)`
+  width: 240px;
+  height: 240px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1rem;
+  font-size: 19px;
+  font-weight: 700;
+  border-radius: 15px;
+  background-color: ${({ theme }) => colors[theme].card};
+  color: ${({ theme }) => colors[theme].font};
+  transition: .3s ease-in;
+  &:hover {
+    transform: translateY(-5px);
+  }
 `;
