5d3c39975debfadc9b471a8a9d91d9a5802c91d6
emachipana <emachipana0102@gmail.com>
2022-06-30 09:49:10 -0500

    ADD: colors & routes in projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,30 @@
+import { SiJavascript, SiRubyonrails } from "react-icons/si";
+import { GrReactjs } from "react-icons/gr";
+import { DiRubyRough } from "react-icons/di";
+
 export const projects = [
   {
-    language: "Ruby",
-    to: "",
-    color: "",
-    icon: ""
+    name: "Ruby Projects",
+    to: "/ruby",
+    color: "#FF2200",
+    icon: <DiRubyRough />
   },
   {
-    language: "Rails",
-    to: "",
-    color: "",
-    icon: ""
+    name: "Rails Projects",
+    to: "/rails",
+    color: "#C71B00",
+    icon: <SiRubyonrails />
   },
   {
-    language: "Javascript",
-    to: "",
-    color: "",
-    icon: ""
+    name: "Javascript Projects",
+    to: "/javascript",
+    color: "#F3DB00",
+    icon: <SiJavascript />
   },
   {
-    language: "React",
-    to: "",
-    color: "",
-    icon: ""
+    name: "React Projects",
+    to: "/react",
+    color: "#61CFEE",
+    icon: <GrReactjs />
   }
-]
+];
