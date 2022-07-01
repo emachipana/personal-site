92347fdb0d16428f3638dcee4778bc0a5a5a5d82
emachipana <emachipana0102@gmail.com>
2022-06-30 19:34:23 -0500

    ADD: render by conditional

--- src/pages/portfolio-page.js
+++ src/pages/portfolio-page.js
@@ -1,10 +1,23 @@
+import { useParams } from "react-router-dom";
 import Projects from "../components/Projects";
+import Ruby from "../components/Projects/Ruby";
 import * as Style from "./styes";
 
 function PortfolioPage() {
+  const params = useParams();
+
   return (
     <Style.MainContainer>
-      <Projects />
+      { 
+        !params.name
+        ?
+        <Projects /> 
+        :
+        (params.name === "ruby" && <Ruby />) ||
+        (params.name === "rails" && <h1>Rails</h1>) ||
+        (params.name === "javascript" && <h1>Javascript</h1>) ||
+        (params.name === "react" && <h1>React</h1>)
+      }
     </Style.MainContainer>
   );
 }
