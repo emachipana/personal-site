2ffcf732acf77aaeb41b6c68143b4c2572b6c6b9
emachipana <emachipana0102@gmail.com>
2022-06-30 23:52:44 -0500

    ADD: styles to contact page

--- src/pages/styes.js
+++ src/pages/styes.js
@@ -25,3 +25,40 @@ export const Container = styled.div`
     margin-top: 10vh;
   }
 `;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: -3.5rem;
+  gap: 2rem;
+  max-width: 620px;
+  background-color: ${({ theme }) => colors[theme].card};
+  border-radius: 15px;
+  padding: 1rem;
+  @media screen and (max-width: 720px) {
+    margin-top: 2rem;
+  }
+`;
+
+export const FormButton = styled.button`
+  width: 30%;
+  margin: 0 1rem;
+  border: none;
+  background-color: ${({ theme }) => colors[theme].font};
+  padding: 0.5rem;
+  color: ${({ theme }) => colors[theme].card};
+  font-size: 17px;
+  font-weight: 600;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  transition: .2s ease-in;
+  &:hover {
+    box-shadow: 0px 0px 4px 2px ${({ theme }) => colors[theme].font};
+  }
+`;
